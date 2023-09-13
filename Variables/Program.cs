4b0cdc4bc8260f07c30650635cb2d54090bcb6b0
Charlie <charlieryancurry@gmail.com>
2023-09-13 14:00:24 -0300

    add "gameplay"

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -14,14 +14,19 @@ namespace Variables
             Console.WriteLine("----------");
             Console.WriteLine();
 
-            //score
-            int score = 0;
+            //declaring variable 'score' as type integer
+            int score;
 
-            Console.WriteLine(score);
-            score += 1;
-            Console.WriteLine(score);
-            score += 1;
-            Console.WriteLine(score);
+            //initializing score
+            score = 0;
+
+            //pretend gameplay
+            score += 100;
+
+            //hud
+            Console.WriteLine("Score: " + score);
+
+            Console.WriteLine();
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey(true);
         }
